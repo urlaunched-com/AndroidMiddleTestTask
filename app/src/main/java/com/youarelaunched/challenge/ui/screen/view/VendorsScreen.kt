f7619834672617147214d2e5645044780ec3c572
Taras Vandzhala <mythbuster.lviv@gmail.com>
2023-08-03 02:51:47 +0300

    Update SearchField to collapse when scroll down

--- app/src/main/java/com/youarelaunched/challenge/ui/screen/view/VendorsScreen.kt
+++ app/src/main/java/com/youarelaunched/challenge/ui/screen/view/VendorsScreen.kt
@@ -12,11 +12,16 @@ import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import com.youarelaunched.challenge.ui.screen.state.VendorsScreenUiState
 import com.youarelaunched.challenge.ui.screen.view.components.ChatsumerSnackbar
+import com.youarelaunched.challenge.ui.screen.view.components.CoordinatedScroll
 import com.youarelaunched.challenge.ui.screen.view.components.EmptySearchResult
 import com.youarelaunched.challenge.ui.screen.view.components.SearchField
 import com.youarelaunched.challenge.ui.screen.view.components.VendorItem
@@ -44,8 +49,8 @@ fun VendorsScreen(
     onTextChange: (String) -> Unit,
     performSearch: () -> Unit
 ) {
-//    val lazyListState = rememberLazyListState()
-//    lazyListState.firstVisibleItemIndex>0|| lazyListState.firstVisibleItemScrollOffset>0
+    val toolbarHeightPx = remember { mutableStateOf(0f) }
+    val topPanelOffset = remember { mutableStateOf(0f) }
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
@@ -53,33 +58,51 @@ fun VendorsScreen(
         backgroundColor = VendorAppTheme.colors.background,
         snackbarHost = { ChatsumerSnackbar(it) }
     ) { paddings ->
-        Column(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            SearchField(
-                searchText = searchText,
-                onTextChange = onTextChange,
-                performSearch = performSearch
-            )
-            if (!uiState.vendors.isNullOrEmpty()) {
-                LazyColumn(
+
+        CoordinatedScroll(
+            collapsingAreaHeightPx = toolbarHeightPx
+        ) { offset, nestedScroll ->
+            topPanelOffset.value = offset
+
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .nestedScroll(nestedScroll)
+            ) {
+                SearchField(
+                    currentOffset = offset,
+                    maxOffset = toolbarHeightPx.value,
                     modifier = Modifier
-                        .padding(paddings)
-                        .fillMaxSize(),
-                    verticalArrangement = Arrangement.spacedBy(16.dp),
-                    contentPadding = PaddingValues(
-                        vertical = 24.dp,
-                        horizontal = 16.dp
-                    )
-                ) {
-                    items(uiState.vendors) { vendor ->
-                        VendorItem(
-                            vendor = vendor
+                        .zIndex(1f)
+                        .fillMaxWidth(),
+                    onHeightCalculated = {
+                        toolbarHeightPx.value = it
+                    },
+                    searchText = searchText,
+                    onTextChange = onTextChange,
+                    performSearch = performSearch
+                )
+
+                if (!uiState.vendors.isNullOrEmpty()) {
+                    LazyColumn(
+                        modifier = Modifier
+                            .padding(paddings)
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.spacedBy(16.dp),
+                        contentPadding = PaddingValues(
+                            vertical = 24.dp,
+                            horizontal = 16.dp
                         )
+                    ) {
+                        items(uiState.vendors) { vendor ->
+                            VendorItem(
+                                vendor = vendor
+                            )
+                        }
                     }
+                } else if (uiState.vendors != null && uiState.vendors.isEmpty()) {
+                    EmptySearchResult()
                 }
-            } else if (uiState.vendors != null && uiState.vendors.isEmpty()) {
-                EmptySearchResult()
             }
         }
     }
